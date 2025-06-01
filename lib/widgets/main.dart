95826058fa0fecf5225f91761da1325c412fc234
abdlerahman ayman <abdelrahmanayman559@gmail.com>
2025-06-02 00:19:17 +0300

    create masarify app widget and create project folders

---
+++ lib/widgets/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:masarify/widgets/masarify_app.dart';
+
+void main() {
+  runApp(const MasarifyApp());
+}
---
+++ lib/widgets/masarify_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MasarifyApp extends StatelessWidget {
+  const MasarifyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
