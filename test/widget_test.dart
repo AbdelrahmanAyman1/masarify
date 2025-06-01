4774a3194a937e7f92a30ff90b395eeb87eaa138
abdlerahman ayman <abdelrahmanayman559@gmail.com>
2025-06-02 00:23:29 +0300

    add flutter_native_splash package

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:masarify/widgets/main.dart';
+import 'package:masarify/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
