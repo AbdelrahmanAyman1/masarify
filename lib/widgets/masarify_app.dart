3e77a7d25fdb95051e3c28c352a65fe4b50d07e4
abdlerahman ayman <abdelrahmanayman559@gmail.com>
2025-06-02 01:40:44 +0300

    finish animated splash screen

--- lib/widgets/masarify_app.dart
+++ lib/widgets/masarify_app.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:masarify/screens/custom_animated_splash_screen.dart';
+import 'package:masarify/screens/home_screen.dart';
 
 class MasarifyApp extends StatelessWidget {
   const MasarifyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: CustomAnimatedSplashScreen.routeName,
+      routes: {
+        CustomAnimatedSplashScreen.routeName:
+            (context) => const CustomAnimatedSplashScreen(),
+        HomeScreen.routeName: (context) => const HomeScreen(),
+      },
+    );
   }
 }
